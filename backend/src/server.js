f28ec3b4dab7794a042f495de35ea5fb2d79bf96
ACaccel <c8763318@gmail.com>
2023-01-03 01:01:54 +0800

    update deployment setting

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,16 +2,29 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import routes from './routes';
+import path from "path";
 
 const app = express();
-const port = process.env.PORT || 4000;
-
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use('/', routes);
+app.use('/api', routes);
 
+if (process.env.NODE_ENV === "production") {
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, "../frontend", "build")));
+    app.get("/*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+    });
+
+    /*app.use(IpFilter(["140.112.0.0/16"], {
+        mode: "allow",
+        detectIp: req => req.connection.remoteAddress?.replace("::ffff:", "") ?? ""
+    }));*/
+}
+
+const port = process.env.PORT || 4000;
 app.listen(port, () =>
     console.log(`Example app listening on port ${port}!`),
 );
